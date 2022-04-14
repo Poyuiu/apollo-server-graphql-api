220518f9e0c89e7d3937c6e7263c1bbd37435588
poyu <yoyohihirobert@gmail.com>
2022-04-15 01:43:06 +0800

    pass all (3) tests

--- graphql/resolvers/version/queries.js
+++ graphql/resolvers/version/queries.js
@@ -3,6 +3,18 @@ import { version } from '../../../config/environment'
 const versionQueries = {
   version: async () => {
     return version
+  },
+  typeInt: async () => {
+    return 1
+  },
+  typeString: async () => {
+    return 'hello'
+  },
+  typeBool: async () => {
+    return true
+  },
+  typeFloat: async () => {
+    return 1.1
   }
 }
 
